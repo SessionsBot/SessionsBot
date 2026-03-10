c8bb8714ba6ecf50b7fb43d48a86475a224e5e0a
br4dyb <halting.credo.00@icloud.com>
2026-03-10 16:09:04 -0500

    fix: add to calendar button logic

--- apps/backend/src/buttons/addToCalendar.ts
+++ apps/backend/src/buttons/addToCalendar.ts
@@ -1,14 +1,15 @@
-import { ActionRowBuilder, ButtonInteraction, ComponentType, ContainerBuilder, MessageFlags, SeparatorBuilder, StringSelectMenuBuilder, TextDisplayBuilder } from "discord.js";
+import { ActionRowBuilder, AttachmentBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, ComponentType, ContainerBuilder, MessageFlags, SectionBuilder, SeparatorBuilder, StringSelectMenuBuilder, TextDisplayBuilder } from "discord.js";
 import { supabase } from "../utils/database/supabase";
 import core from "../utils/core/core";
 import { getSubscriptionFromInteraction } from "@sessionsbot/shared";
-import { defaultFooterText } from "../utils/bot/messages/basic";
+import { defaultFooterText, genericErrorMsg } from "../utils/bot/messages/basic";
 import { DateTime } from "luxon";
 import { URLS } from "../utils/core/urls";
+import { google, ics, outlook, yahoo, aol, office365, type CalendarEvent, outlookMobile, office365Mobile } from 'calendar-link'
+import { useLogger } from "../utils/logs/logtail";
 
-const {
-    colors: { getOxColor },
-} = core
+// Logger Util:
+const createLog = useLogger()
 
 export default {
     data: {
@@ -28,15 +29,25 @@ export default {
         const { data: session, error: sessionError } = await supabase.from('sessions')
             .select('*')
             .eq('id', sessionId)
-            .select()
             .single()
         if (sessionError) throw sessionError;
+        if (!session) {
+            // Session NOT FOUND - Alert & Log:
+            createLog.for('Database').warn('Cannot find session for "Add to Calendar" interaction!', { guildId: i?.guildId, userId: i?.user?.id, sessionId, session })
+            return i.editReply({
+                components: [genericErrorMsg({
+                    title: `${core.emojis.string('warning')}  Error - Cannot Find Session!`,
+                    reasonDesc: `It's possible this session data is no longer accessible/outdated. \n-# **Session UID**: \`${sessionId}\`\n-# Not right? Get in touch with Bot Support!`
+                })]
+            })
+        }
 
-        // Build/Send Option Menu:
+
+        // Build & Send Calendar Option Menu:
         const optionMsg = new ContainerBuilder({
-            accent_color: getOxColor('blue'),
+            accent_color: core.colors.getOxColor('blue'),
             components: <any>[
-                new TextDisplayBuilder({ content: `### ${core.emojis.string('calendar')} Add Session to Calendar \n-# Please select from an available calendar format to continue.` }),
+                new TextDisplayBuilder({ content: `## ${core.emojis.string('calendar')}  Add Session to Calendar \n> Please select from an available calendar format to continue...` }),
                 new SeparatorBuilder(),
                 new ActionRowBuilder({
                     components: [
@@ -51,13 +62,28 @@ export default {
                                 },
                                 {
                                     label: 'Google Calendar',
-                                    description: 'Direct Create URL',
+                                    description: 'Direct Creation URL',
                                     value: 'google'
                                 },
                                 {
                                     label: 'Outlook Calendar',
-                                    description: 'Direct Create URL',
+                                    description: 'Direct Creation URL',
                                     value: 'outlook'
+                                },
+                                {
+                                    label: 'Yahoo',
+                                    description: 'Direct Creation URL',
+                                    value: 'yahoo'
+                                },
+                                {
+                                    label: 'AOL',
+                                    description: 'Direct Creation URL',
+                                    value: 'aol'
+                                },
+                                {
+                                    label: 'Office 365',
+                                    description: 'Direct Creation URL',
+                                    value: 'office365'
                                 },
                                 {
                                     label: 'Raw ICS Format',
@@ -67,18 +93,17 @@ export default {
                             ]
                         })
                     ]
-                })
+                }),
+                // Add Watermark - IF Plan Limits Allow:
+                ...subscription?.limits?.SHOW_WATERMARK
+                    ? [
+                        new SeparatorBuilder(),
+                        defaultFooterText()
+                    ]
+                    : [],
 
             ]
         })
-
-        // Add Watermark:
-        if (subscription.limits.SHOW_WATERMARK) {
-            optionMsg.components.push(
-                new SeparatorBuilder(),
-                defaultFooterText()
-            )
-        }
 
         // Send Option Menu:
         const intMsg = await i.editReply({
@@ -88,8 +113,9 @@ export default {
 
         // Create response collector:
         const replyCollector = intMsg.createMessageComponentCollector({
-            idle: 60000,
+            idle: 45_000,
             componentType: ComponentType.StringSelect,
+            filter: (ci) => ci.user.id == i.user.id
         })
 
         // On Reply Collector - Interaction Collected:
@@ -98,106 +124,182 @@ export default {
             const selected = ci.values?.at(-1)
             await ci.deferUpdate()
 
-            // Get Session Dates:
+            // Get Session Data/Dates:
             const startUtc = DateTime.fromISO(session.starts_at_utc, { zone: 'utc' })
             const endUtc = session.duration_ms
                 ? DateTime.fromISO(session.starts_at_utc, { zone: 'utc' }).plus({ millisecond: session.duration_ms })
-                : null
-            // Util: Convert DateTime -> Cal Date Format:
-            function formatDate(d: DateTime) {
-                return d.toFormat(`yyyyMMdd'T'HHmmss'Z'`);
-            }
-            // Util: Build Google Calendar Event URL:
-            const buildGoogleUrl = () => {
-                const params = new URLSearchParams({
-                    action: 'TEMPLATE',
-                    text: session.title,
-                    details: session.description ?? '',
-                    location: session.url ?? '',
-                    dates: endUtc
-                        ? `${formatDate(startUtc)}/${formatDate(endUtc)}`
-                        : `${startUtc.toFormat(`yyyyMMdd`)}/${startUtc.plus({ day: 1 }).toFormat(`yyyyMMdd`)}`,
-                })
-                return `https://calendar.google.com/calendar/render?${params.toString()}`
-            }
-            // Util: Build Outlook Calendar Event URL:
-            const buildOutlookUrl = () => {
-                const params = new URLSearchParams({
-                    subject: session.title,
-                    body: session.description ?? '',
-                    location: session.url ?? '',
-                    startdt: startUtc.toISO(),
-                    ...(endUtc && { enddt: endUtc.toISO() })
-                })
-                return `https://outlook.live.com/calendar/0/deeplink/compose?${params.toString()}`
+                : DateTime.fromISO(session.starts_at_utc, { zone: 'utc' }).plus({ hour: 1 })
+
+            const eventData: CalendarEvent = {
+                uid: session?.id,
+                title: session?.title,
+                description: session?.description,
+                start: startUtc?.toISO(),
+                end: endUtc?.isValid
+                    ? endUtc?.toISO()
+                    : undefined,
+                busy: true,
+                url: session?.url ?? undefined
             }
 
-            const buildICS = () => {
-                return `
-                    BEGIN:VCALENDAR
-                    VERSION:2.0
-                    PRODID:-//Sessions Bot//EN
-                    CALSCALE:GREGORIAN
-                    BEGIN:VEVENT
-                    UID:${session.id}@sessionsbot.fyi
-                    DTSTAMP:${formatDate(DateTime.now())}
-                    DTSTART:${formatDate(startUtc)}
-                    ${endUtc ? `DTEND:${formatDate(endUtc)}` : ''}
-                    SUMMARY:${session.title}
-                    DESCRIPTION:${session.description ?? ''}
-                    LOCATION:${session.url ?? ''}
-                    END:VEVENT
-                    END:VCALENDAR
-                `.trim()
-            }
-
-
-
-
-            const responseData = () => {
+            // Get Selected Calendar Format:
+            const getCalFormat = (): {
+                desktop: string,
+                mobile: string | undefined,
+                title: string,
+                isFileFormat: boolean,
+            } => {
                 switch (selected) {
                     case 'outlook':
-                        return buildOutlookUrl()
+                        return {
+                            desktop: outlook(eventData),
+                            mobile: outlookMobile(eventData),
+                            title: 'Outlook',
+                            isFileFormat: false
+                        }
                     case 'google':
-                        return buildGoogleUrl()
+                        return {
+                            desktop: google(eventData),
+                            mobile: undefined,
+                            title: 'Google',
+                            isFileFormat: false
+                        }
+                    case 'aol':
+                        return {
+                            desktop: aol(eventData),
+                            mobile: undefined,
+                            title: 'AOL',
+                            isFileFormat: false
+                        }
+                    case 'yahoo':
+                        return {
+                            desktop: yahoo(eventData),
+                            mobile: undefined,
+                            title: 'Yahoo',
+                            isFileFormat: false
+                        }
+                    case 'office365':
+                        return {
+                            desktop: office365(eventData),
+                            mobile: office365Mobile(eventData),
+                            title: 'Office 365',
+                            isFileFormat: false
+                        }
                     case 'apple':
+                        return {
+                            desktop: ics(eventData),
+                            mobile: undefined,
+                            title: 'Apple',
+                            isFileFormat: true
+                        }
                     case 'ics':
                     default:
-                        return `\`\`\`ics\n${buildICS()}\n\`\`\``
+                        return {
+                            desktop: ics(eventData),
+                            mobile: undefined,
+                            title: '.ics File',
+                            isFileFormat: true
+                        }
                 }
             }
+            const calResult = getCalFormat();
 
+
+
+            // Send Downloadable - ICS Format:
             await i.editReply({
                 components: [
                     new ContainerBuilder({
-                        accent_color: getOxColor('success'),
+                        accent_color: core.colors.getOxColor('success'),
                         components: <any>[
-                            new TextDisplayBuilder({ content: `### ${core.emojis.string('success')} - Selection Received \n-# You're selected calendar format is provided below.` }),
-                            new SeparatorBuilder(),
-                            new TextDisplayBuilder({ content: `${responseData()}` }),
+                            new TextDisplayBuilder({ content: `### ${core.emojis.string('success')}  Calendar Format Selected! \n> You're selected calendar format is provided below. \n> -# **Format Type**: \`${calResult?.title}\`` }),
+                            // Add Creation Link Buttons - If applicable:
+                            ...!calResult.isFileFormat
+                                ? [
+                                    new SeparatorBuilder(),
+                                    new ActionRowBuilder({
+                                        components: [
+                                            new ButtonBuilder({
+                                                style: ButtonStyle.Link,
+                                                label: `Add to Calendar ${calResult?.mobile != undefined ? '(desktop)' : ''}`,
+                                                emoji: {
+                                                    id: core.emojis.ids.calendar
+                                                },
+                                                url: calResult.desktop
+                                            }),
+                                            ...calResult?.mobile != undefined
+                                                ? [
+                                                    new ButtonBuilder({
+                                                        style: ButtonStyle.Link,
+                                                        label: `Add to Calendar ${calResult?.mobile != undefined ? '(mobile)' : ''}`,
+                                                        emoji: {
+                                                            id: core.emojis.ids.calendar
+                                                        },
+                                                        url: calResult.mobile
+                                                    })
+                                                ]
+                                                : []
+                                        ]
+                                    })
+                                ]
+                                : [],
+                            // Add Watermark - If applicable:
+                            ...subscription?.limits?.SHOW_WATERMARK
+                                ? [
+                                    new SeparatorBuilder(),
+                                    defaultFooterText()
+                                ]
+                                : []
                         ]
                     })
                 ]
             })
+            if (calResult.isFileFormat) {
+                const rawICS = decodeURIComponent(
+                    calResult?.desktop?.split(",")[1]
+                )
+                const file = new AttachmentBuilder(
+                    Buffer.from(rawICS),
+                    { name: `session-${session.id}.ics` }
+                )
+                await i.followUp({
+                    flags: MessageFlags.Ephemeral,
+                    content: 'Universal Calendar Event - `.ics` format',
+                    files: [file]
+                })
+            }
+
+
+
 
             // End the collector
-            replyCollector.stop()
+            replyCollector.stop('responded')
+
         })
+
 
         // On Reply Collector - End/Timeout:
         replyCollector.on('end', async (v, r) => {
-            console.info('Collector Ended!', r)
-            if (r == 'idle') {
-                // Send Timed Out Alert:
+            if (r == 'idle') { // Send Timed Out Alert:
                 await i.editReply({
                     components: [
                         new ContainerBuilder({
-                            accent_color: getOxColor('warning'),
+                            accent_color: core.colors.getOxColor('warning'),
                             components: <any>[
                                 new TextDisplayBuilder({ content: `### ${core.emojis.string('timeout')} - Timed Out! \n-# Unfortunately you ran out of time to respond to this interaction. \n-# You'll have to start over and try again.` }),
                                 new SeparatorBuilder(),
                                 new TextDisplayBuilder({ content: `-# [Support Resources](${URLS.site_links.support})` }),
                             ]
+                        })
+                    ]
+                })
+            } else if (r != 'responded') { // Send Unknown Error Alert - Collector Canceled:
+                createLog.for('Bot').warn('Add to Calendar - Response interaction collector ended - Unknown reason!', { guildId: i?.guildId, userId: i?.user?.id, reason: r })
+                await i.editReply({
+                    components: [
+                        genericErrorMsg({
+                            title: `${core.emojis.string('warning')}  Interaction Response Collector - Failed!`,
+                            reasonDesc: `It appears that while trying to record your response for a calendar format selection, We ran into an unknown error! \n-# If this issue persists, please contact [Bot Support](${URLS.support_chat})!`
                         })
                     ]
                 })
