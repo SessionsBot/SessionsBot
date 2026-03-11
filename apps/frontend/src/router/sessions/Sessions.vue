06b602f34fa3ad4665885a542487b1a25d33e17f
br4dyb <halting.credo.00@icloud.com>
2026-03-11 11:16:19 -0500

    wip: session view UI

--- apps/frontend/src/router/sessions/Sessions.vue
+++ apps/frontend/src/router/sessions/Sessions.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
     import { useAuthStore } from '@/stores/auth';
     import useDashboardStore from '@/stores/dashboard/dashboard';
+    import { externalUrls } from '@/stores/nav';
     import { supabase } from '@/utils/supabase';
     import { toHTML } from '@odiffey/discord-markdown';
     import { processVariableText } from '@sessionsbot/shared';
     import { DateTime } from 'luxon';
     import z from 'zod';
+    import SessionDetailsCard from './components/SessionDetailsCard.vue';
 
 
     // Services:
@@ -18,6 +20,7 @@
     const sessionIdInvalid = ref(false)
 
     const sessionData = useAsyncState(async (id: string) => {
+        await new Promise<void>((r) => setTimeout(() => r(), 1_500))
         if (!id) throw new Error('No Session ID for data fetch provided!')
         const { data, error } = await supabase.from('sessions')
             .select('*, session_rsvp_slots(*, session_rsvps(*))')
@@ -32,51 +35,6 @@
         },
     })
     const s = computed(() => sessionData.state.value)
-
-    const startDateUTC = computed(() => DateTime.fromISO(String(s?.value?.starts_at_utc), { zone: 'utc' }))
-
-    const parsedDescription = computed(() => {
-        const raw = s.value?.description
-        if (!raw) return null
-        const processed = processVariableText(raw, { displayDate: startDateUTC.value.setZone(s?.value?.time_zone) })
-        const html = toHTML(processed, {
-            embed: true,
-            discordOnly: false,
-            discordCallback: {
-                role(node) {
-                    return `&Role`
-                },
-                channel(node) {
-                    return `#Channel`
-                },
-                user(node) {
-                    return `@User`
-                },
-                timestamp(node) {
-                    if (!isNaN(node.timestamp)) {
-                        if (node.style == 'R') {
-                            return DateTime.fromSeconds(Number(node.timestamp)).toRelative() ?? "TIMESTAMP"
-                        }
-                        const styleToken = () => {
-                            if (node.style == 't') return 't'
-                            else if (node.style == 'T') return 'tt'
-                            else if (node.style == 'd') return 'D'
-                            else if (node.style == 'D') return 'DDD'
-                            else if (node.style == 'f') return `DDD 'at' t`
-                            else if (node.style == 'F') return `DDD 'at' t`
-                            else return 'f'
-                        }
-                        return DateTime.fromSeconds(Number(node.timestamp)).toFormat(styleToken())
-                    } else return "TIMESTAMP"
-
-                },
-                slash(node) {
-                    return '/command'
-                }
-            },
-        })
-        return html
-    })
 
     onMounted(() => {
         // Validate & Load Session by Id:
@@ -101,80 +59,97 @@
 
 
 <template>
-    <main class="flex justify-center items-center flex-col gap-2 p-4">
+    <main class="flex relative justify-center items-center flex-col gap-2 p-4">
 
-        <Transition name="zoom">
+        <!-- Back to Dashboard - Breadcrumb -->
+        <div class="absolute! group/bc hover:text-brand-2 cursor-pointer flex w-fit h-fit top-3.5! left-3.5! z-3!">
 
-            <!-- Invalid Id - Card -->
-            <div v-if="sessionIdInvalid" class="p-3 rounded-md bg-bg-2 border-2 border-ring-soft flex flex-center">
-                INVALID SESSION ID:
-                {{ rawId }}
-            </div>
+            <RouterLink to="/dashboard" class="flex w-fit h-fit flex-center gap-1.5 text-sm flex-row">
+                <Iconify icon="mynaui:arrow-long-left" class="group-hover/bc:-translate-x-0.75 transition-all"
+                    size="20" />
+                <p class="opacity-65"> Go to Dashboard </p>
+            </RouterLink>
 
+        </div>
 
-            <!-- Session Data View -->
-            <div v-else class="flex flex-center p-5">
-
-                <div class="p-4 rounded-md bg-bg-2 border-2 border-ring-soft flex gap-2 flex-center flex-col">
-
-                    <!-- Title & Description -->
-                    <div>
-                        <!-- Title -->
-                        <p class="w-full text-xl font-bold">
-                            {{ s?.title }}
-                        </p>
-
-                        <!-- Description -->
-                        <span v-if="parsedDescription"
-                            class="w-full text-sm opacity-65 font-bold flex items-start justify-start">
-                            <span class="discord-preview w-full" v-html="parsedDescription" />
-                        </span>
-
-                    </div>
-
-                    <!-- Start Date -->
-                    <div class="flex w-full flex-col gap-1">
-                        <span class="flex items-center gap-px">
-                            <Iconify icon="mdi:clock" class="opacity-80" size="20" />
-                            <p> Start Date </p>
-                        </span>
-                        <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5">
-                            {{ DateTime.fromISO(String(s?.starts_at_utc)).toFormat('f') ?? 'Unknown' }}
-                        </span>
-                    </div>
-
-                    <!-- End Date -->
-                    <div v-if="s?.duration_ms && Number(s?.duration_ms)" class="flex w-full flex-col gap-1">
-                        <span class="flex items-center gap-px">
-                            <Iconify icon="mdi:clock" class="opacity-80" size="20" />
-                            <p> End Date </p>
-                        </span>
-                        <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5">
-                            {{ DateTime.fromISO(String(s?.starts_at_utc)).plus({
-                                millisecond: Number(s?.duration_ms)
-                            }).toFormat('f') ?? 'Unknown' }}
-                        </span>
-                    </div>
-
-                    <!-- Time Zone -->
-                    <div class="flex w-full flex-col gap-1">
-                        <span class="flex items-center gap-px">
-                            <Iconify icon="mdi:clock" class="opacity-80" size="20" />
-                            <p> Time Zone </p>
-                        </span>
-                        <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5">
-                            {{ s?.time_zone ?? 'Unknown' }}
-                        </span>
-                    </div>
+        <!-- Main Content - Wrap -->
+        <div class="flex flex-center grow w-full h-full p-10 text-center">
+            <Transition name="zoom" mode="out-in">
 
 
+                <!-- Invalid Id - Card -->
+                <div v-if="sessionIdInvalid" class="card p-7 flex-col">
+                    <span class="flex flex-center gap-1.5 flex-row text-xl p-2">
+                        <Iconify icon="mdi:warning" size="26" />
+                        <p class="font-bold"> Invalid Session ID! </p>
+                    </span>
+
+                    <p class="opacity-75 font-semibold text-sm">
+                        Please confirm the Session ID you're trying to view!
+                    </p>
+                    <p class="text-xs italic opacity-55 px-4 mt-4">
+                        Requested UID: {{ rawId ?? '?' }}
+                    </p>
+                    <RouterLink to="/support"
+                        class="text-xs italic opacity-35 hover:opacity-70 transition-all mt-2 hover:underline px-4">
+                        Need Help?
+                    </RouterLink>
                 </div>
 
-            </div>
-        </Transition>
+
+                <!-- Failure Fetching - Card -->
+                <div v-else-if="sessionData.error.value != null" class="card p-7 flex-col">
+                    <span class="flex flex-center gap-1.5 flex-row text-xl p-2">
+                        <Iconify icon="mdi:warning" size="26" class="animate-spin" />
+                        <p class="font-bold"> Invalid Session ID! </p>
+                    </span>
+
+                    <p class="opacity-75 font-semibold text-sm">
+                        Please confirm your...
+                    </p>
+                    <p class="text-xs italic opacity-55 px-4 mt-4">
+                        Requested UID: {{ rawId ?? '?' }}
+                    </p>
+                </div>
+
+
+                <!-- Loading Session - Card -->
+                <div v-else-if="!sessionData.isReady.value || sessionData.isLoading.value" class="card p-7 flex-col">
+                    <span class="flex flex-center gap-1.5 flex-row text-xl p-2">
+                        <Iconify icon="mingcute:loading-3-line" size="26" class="animate-spin" />
+                        <p class="font-bold"> Loading </p>
+                    </span>
+
+                    <p class="opacity-75 font-semibold text-sm">
+                        Please wait while we fetch this sessions details...
+                    </p>
+                    <p class="text-xs italic opacity-55 px-4 mt-4">
+                        Session UID: {{ rawId ?? '?' }}
+                    </p>
+                </div>
+
+
+                <!-- Session - Data View - Card -->
+                <SessionDetailsCard v-else :session="s" />
+
+            </Transition>
+        </div>
+
+
+
+        <SiteFooter class="py-4" />
 
     </main>
 </template>
 
 
-<style scoped></style>
+<style scoped>
+
+    @reference "@/styles/main.css";
+
+    :deep(.card) {
+        @apply p-3 max-w-[80%] m-7 rounded-md bg-bg-2 border-2 border-ring-soft flex flex-center;
+    }
+
+
+</style>
---
+++ apps/frontend/src/router/sessions/components/SessionDetailsCard.vue
@@ -0,0 +1,148 @@
+<script lang="ts" setup>
+    import { useAuthStore } from '@/stores/auth';
+    import { toHTML } from '@odiffey/discord-markdown'
+    import { processVariableText, type Database, type FullSessionData } from '@sessionsbot/shared'
+    import { DateTime } from 'luxon'
+
+    // Incoming Props:
+    const props = defineProps<{
+        session: Database['public']['Tables']['sessions']['Row'] | FullSessionData | null
+    }>()
+    // Session Prop - Shorthand
+    const s = computed(() => props.session)
+
+    // Services:
+    const auth = useAuthStore();
+
+    // Session Start UTC
+    const startDateUTC = computed(() => DateTime.fromISO(String(s?.value?.starts_at_utc), { zone: 'utc' }))
+
+    // Session Description (if any)
+    const parsedDescription = computed(() => {
+        const raw = s.value?.description
+        if (!raw) return null
+        const processed = processVariableText(raw, { displayDate: startDateUTC.value.setZone(s?.value?.time_zone) })
+        const html = toHTML(processed, {
+            embed: true,
+            discordOnly: false,
+            discordCallback: {
+                role(node) { return `@Role` },
+                channel(node) { return `#Channel` },
+                user(node) { return `@User` },
+                slash(node) { return '/command' },
+                timestamp(node) {
+                    if (!isNaN(node.timestamp)) {
+                        if (node.style == 'R') {
+                            return DateTime.fromSeconds(Number(node.timestamp))?.toRelative() ?? "TIMESTAMP"
+                        }
+                        const styleToken = () => {
+                            if (node.style == 't') return 't'
+                            else if (node.style == 'T') return 'tt'
+                            else if (node.style == 'd') return 'D'
+                            else if (node.style == 'D') return 'DDD'
+                            else if (node.style == 'f') return `DDD 'at' t`
+                            else if (node.style == 'F') return `DDD 'at' t`
+                            else return 'f'
+                        }
+                        return DateTime.fromSeconds(Number(node.timestamp))?.toFormat(styleToken())
+                    } else return "TIMESTAMP"
+
+                }
+            }
+        })
+        return html
+    })
+
+</script>
+
+
+<template>
+    <div class="flex flex-center p-5 grow">
+
+        <div class="p-4 rounded-md bg-bg-2 border-2 border-ring-soft flex gap-2 flex-center flex-col">
+
+            <!-- Title - Header -->
+            <div class="flex flex-center flex-col gap-2">
+                <!-- Title -->
+                <p class="w-full text-2xl font-bold">
+                    {{ s?.title }}
+                </p>
+
+                <!-- Guild Icon -->
+                <img :src="'/discord.png'" class="size-24 m-4 rounded-md border-2 border-ring-soft" </div>
+
+
+                <!-- Description -->
+                <div v-if="parsedDescription" class="flex w-full flex-col gap-1">
+                    <span class="flex items-center gap-px">
+                        <Iconify icon="mdi:text" class="opacity-80" size="20" />
+                        <p> Description </p>
+                    </span>
+                    <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5 block! discord-preview"
+                        v-html="parsedDescription" />
+                </div>
+
+                <!-- Start Date -->
+                <div class="flex w-full flex-col gap-1">
+                    <span class="flex items-center gap-px">
+                        <Iconify icon="mdi:clock" class="opacity-80" size="20" />
+                        <p> Start Date </p>
+                    </span>
+                    <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5">
+                        {{ DateTime.fromISO(String(s?.starts_at_utc)).toFormat('f') ?? 'Unknown' }}
+                    </span>
+                </div>
+
+                <!-- End Date -->
+                <div v-if="s?.duration_ms && Number(s?.duration_ms)" class="flex w-full flex-col gap-1">
+                    <span class="flex items-center gap-px">
+                        <Iconify icon="mdi:clock" class="opacity-80" size="20" />
+                        <p> End Date </p>
+                    </span>
+                    <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5">
+                        {{ DateTime.fromISO(String(s?.starts_at_utc)).plus({
+                            millisecond: Number(s?.duration_ms)
+                        }).toFormat('f') ?? 'Unknown' }}
+                    </span>
+                </div>
+
+                <!-- Time Zone -->
+                <div class="flex w-full flex-col gap-1">
+                    <span class="flex items-center gap-px">
+                        <Iconify icon="mdi:clock" class="opacity-80" size="20" />
+                        <p> Time Zone </p>
+                    </span>
+                    <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5">
+                        {{ s?.time_zone ?? 'Unknown' }}
+                    </span>
+                </div>
+
+                <!-- RSVPS -->
+                <div class="flex w-full flex-col gap-1">
+                    <span class="flex items-center gap-px">
+                        <Iconify icon="mdi:user-check" class="opacity-80" size="20" />
+                        <p> RSVPs </p>
+                    </span>
+                    <span class="ml-2 bg-bg-3 rounded-md p-1 px-1.5 flex flex-center">
+
+                        <Button v-if="!auth.signedIn" unstyled
+                            class="button-base button-primary active:scale-95 py-0.5 my-1">
+                            <DiscordIcon class="scale-90" />
+                            Sign in to view
+                        </Button>
+
+                        <p class="opacity-55 italic text-xs py-12">
+                            (rsvps here)
+                        </p>
+
+                    </span>
+                </div>
+
+
+            </div>
+
+        </div>
+</template>
+
+
+<style scoped></style>
